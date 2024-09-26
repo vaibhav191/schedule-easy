159dd03da9acc6838903171ed4faab77d068ddfa
root <root@VaibhavEnvy>
2024-09-25 20:37:32 -0700

    fix
    added pvt key store to the handler

--- auth_service/handlers/key_handler.py
+++ auth_service/handlers/key_handler.py
@@ -1,24 +1,9 @@
+from typing import Tuple
 from ..models.keys import Keys
 from .crypto_handler import CryptoHandler
 
 
 class KeyHandler:
-    """
-    A class to manage and retrieve cryptographic keys.
-
-    Attributes:
-    -----------
-    keys : dict
-        A dictionary that maps key names (from the Keys enum) to their corresponding cryptographic keys.
-
-    Methods:
-    --------
-    __init__():
-        Initializes the KeyWallet with a dictionary of keys set to None.
-    
-    get_key(key_name: Keys) -> bytes:
-        Retrieves the public key for the given key name. If the key is not already cached, it fetches the key using the CryptoHandler(which requests the same from crypto_service) and stores it in the keys dictionary.
-    """
     def __init__(self):
         self.pub_keys = {x.name:None for x in Keys}
         self.pvt_keys = {x.name for x in (Keys.REFRESH_TOKEN, Keys.JWT_TOKEN)}
@@ -26,8 +11,24 @@ class KeyHandler:
         if not self.pub_keys[key_name.name]:
             self.pub_keys[key_name.name] = CryptoHandler.get_public_key(key_name)
         return self.keys[key_name.name]
-    def get_pvt_key(self, key_name: Keys) -> bytes:
+    def get_pvt_key(self, key_name: Keys) -> Tuple[bytes, bytes]:
+        """
+        Retrieves the private key and its associated password for a given key name.
+
+        Args:
+            key_name (Keys): The name of the key to retrieve.
+
+        Returns:
+            Tuple[bytes, bytes]: A tuple containing the private key and its password.
+
+        Raises:
+            Exception: If the provided key_name is not valid.
+        """
         if key_name.name not in self.pvt_keys:
             print("Invalid key_name:", key_name)
             raise Exception("Invalid key_name")
-        return CryptoHandler.get_private_key(key_name)
+        if not self.pub_keys[key_name.name]:
+            key, password = CryptoHandler.get_private_key(key_name)
+            self.pvt_keys[key_name.name]['key'] = key
+            self.pvt_keys[key_name.name]['password'] = password
+        return self.keys[key_name.name]
