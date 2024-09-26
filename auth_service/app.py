e0b3ab3010adf0c3accd82c7ad48c3b8a0dc4ae3
root <root@VaibhavEnvy>
2024-09-25 17:42:06 -0700

    refactor
    Updated KeyWallet to also save private keys related to refresh or jwt token

--- auth_service/app.py
+++ auth_service/app.py
@@ -688,11 +688,18 @@ class KeyWallet:
         Retrieves the public key for the given key name. If the key is not already cached, it fetches the key using the CryptoHandler(which requests the same from crypto_service) and stores it in the keys dictionary.
     """
     def __init__(self):
-        self.keys = {x.name:None for x in Keys}
-    def get_key(self, key_name: Keys) -> bytes:
-        if not self.keys[key_name.name]:
-            self.keys[key_name.name] = CryptoHandler.get_public_key(key_name)
+        self.pub_keys = {x.name:None for x in Keys}
+        self.pvt_keys = {x.name for x in (Keys.REFRESH_TOKEN, Keys.JWT_TOKEN)}
+    def get_pub_key(self, key_name: Keys) -> bytes:
+        if not self.pub_keys[key_name.name]:
+            self.pub_keys[key_name.name] = CryptoHandler.get_public_key(key_name)
         return self.keys[key_name.name]
+    def get_pvt_key(self, key_name: Keys) -> bytes:
+        if key_name.name not in self.pvt_keys:
+            print("Invalid key_name:", key_name)
+            raise Exception("Invalid key_name")
+        return CryptoHandler.get_private_key(key_name)
+
 
 key_wallet = KeyWallet()
 
