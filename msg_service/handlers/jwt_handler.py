83043be1ce8434ed5aec828431fae2bf0de9d7b4
root <root@VaibhavEnvy>
2024-10-02 16:11:34 -0700

    feat
    1. Validate token for the message queue publisher.
    2. Hashing for the message using AWS KMS.
    3. Dockerfile

---
+++ msg_service/handlers/jwt_handler.py
@@ -0,0 +1,65 @@
+'''
+    -> Message body: 
+        # RFC 7519 - Registered Claim Names
+        iss (Issuer)
+        sub (Subject)
+        aud (Audience)
+        exp (Expiration Time)
+        nbf (Not Before)
+        iat (Issued At)
+        jti (JWT ID)
+        
+        # private claims
+        cdi (Credentials ID) 
+        atkn (Auth Token)
+'''
+
+# Implementation needed. Use JWT library to encrypt? Do not send sensitive details. Save in cookie httponly.
+import datetime
+from logging import Logger
+from typing import Tuple
+import jwt
+from cryptography.hazmat.primitives import serialization
+
+class JWTHandler:
+    @staticmethod
+    def validate_jwt_token(jwt_token: str, jwt_pub_key: bytes, logger: Logger):
+        try:
+            data = jwt.decode(jwt_token, jwt_pub_key, algorithms=['RS256'], issuer="schedule-easy/auth-service", audience="schedule-easy/*")
+            if data['tkn'] != "auth token":
+                logger.debug(f"{__class__.__name__}: Invalid token type, expected auth token, received: {data['tkn']}")
+                return False
+        except jwt.ExpiredSignatureError:
+            logger.debug(f"{__class__.__name__}: Session Expired")
+            return False
+        except jwt.InvalidAudienceError:
+            logger.debug(f"{__class__.__name__}: Invalid Audience")
+            return False
+        except jwt.InvalidIssuerError:
+            logger.debug(f"{__class__.__name__}: Invalid Issuer")
+            return False
+        except jwt.InvalidIssuedAtError:
+            logger.debug(f"{__class__.__name__}: Invalid Issued At")
+            return False
+        return True
+
+    @staticmethod
+    def validate_refresh_token(refresh_token: str, refresh_pub_key: bytes, logger: Logger):
+        try:
+            data = jwt.decode(refresh_token, refresh_pub_key, algorithms=['RS256'], issuer="schedule-easy/auth-service", audience="schedule-easy/*")
+            if data['tkn'] != "refresh token":
+                logger.debug(f"{__class__.__name__}: Invalid token type, expected refresh token, received: {data['tkn']}")
+                return False
+        except jwt.ExpiredSignatureError:
+            logger.debug(f"{__class__.__name__}: Session Expired")
+            return False
+        except jwt.InvalidIssuerError:
+            logger.debug(f"{__class__.__name__}: Invalid Issuer")
+            return False
+        except jwt.InvalidIssuedAtError:
+            logger.debug(f"{__class__.__name__}: Invalid Issued At")
+            return False
+        except jwt.InvalidAudienceError:
+            logger.debug(f"{__class__.__name__}: Invalid Audience")
+            return False
+        return True
---
+++ msg_service/handlers/kms_handler.py
@@ -0,0 +1,34 @@
+import os
+import boto3
+# AWS KMS
+class KMSHandler:
+    '''
+        Remember, KMSHandler does not need KMS Access and secret key when running on AWS through IAM roles.
+    '''
+    def __init__(self) -> None:
+        self.access_key = os.getenv('AUTH_KMS_ACCESS_KEY')
+        self.secret_key = os.getenv('AUTH_KMS_SECRET_KEY')
+        self.region = os.getenv('AUTH_KMS_REGION')
+        # keys
+        self.google_auth_credentials_keyId = os.getenv('AUTH_APP_CREDENTIALS_KEYID')
+        self.eventQ_mac_keyId = os.getenv('MSG_APP_MAC_KEYID')
+        
+        self.client = boto3.client('kms', region_name = self.region, aws_access_key_id = self.access_key, aws_secret_access_key = self.secret_key)
+    
+    def encrypt(self, data: bytes, keyId) -> bytes:
+        resp = self.client.encrypt(KeyId = keyId, Plaintext = data, EncryptionContext = {'context': 'google_app_cred'})
+        if 'CiphertextBlob' in resp:
+            return resp['CiphertextBlob']
+        return False
+
+    def decrypt(self, data: bytes, keyId) -> bytes:
+        resp = self.client.decrypt(CiphertextBlob = data, KeyId = keyId, EncryptionContext = {'context': 'google_app_cred'})
+        if 'Plaintext' in resp:
+            return resp['Plaintext']
+        return False
+
+    def generate_hmac(self, data: bytes, keyId) -> bytes:
+        resp = self.client.generate_mac(Message = data, KeyId = keyId, MacAlgorithm = 'HMAC_SHA_256', DryRun = False)
+        if 'Mac' in resp:
+            return resp['Mac']
+        return False
